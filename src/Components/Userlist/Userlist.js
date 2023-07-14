a024491eb91d47720e626b09d7b2f3619dc28e82
Harsh <evolvingdev10@gmail.com>
2023-07-14 15:31:02 +0530

    warnings removed

--- src/Components/Userlist/Userlist.js
+++ src/Components/Userlist/Userlist.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Userlist.css";
-import axios from "axios";
 
 function Userlist(props) {
   console.log(props);
